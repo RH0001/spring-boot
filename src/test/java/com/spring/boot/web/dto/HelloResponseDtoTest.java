11bb2697b023b7ebd4b267aee39a48f84a9fa586
kwh_mac <kwon.wh02@gmail.com>
2021-12-04 15:23:58 +0900

    100% - 스프링_부트에서_테스트_코드를_작성하자

---
+++ src/test/java/com/spring/boot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,24 @@
+package com.spring.boot.web.dto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lombok_test() {
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto  = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
